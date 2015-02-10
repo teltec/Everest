ec8a8edeb35dabca444773f1c49e157b344a96c6
jweyrich <jweyrich@gmail.com>
2015-02-10 12:42:23 -0800

    Disable CLSCompliant.

--- Teltec.Data/Properties/AssemblyInfo.cs
+++ Teltec.Data/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // https://msdn.microsoft.com/en-us/library/system.clscompliantattribute.aspx
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
