e1db59759455f9b50c6784616ce6866cc1ebe643
Jardel Weyrich <jweyrich@gmail.com>
2015-04-28 15:47:17 -0300

    - Create basic service to execute scheduled plans; - Create ClickOnce manifest and enable its security settings;

---
+++ Teltec.Backup.Svc/Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace Teltec.Backup.Svc
+{
+	public partial class Service : ServiceBase
+	{
+		static Timer timer;
+
+		static void Main()
+		{
+			System.ServiceProcess.ServiceBase[] servicesToRun; 
+			servicesToRun = new ServiceBase[] { new Service() };
+
+			System.ServiceProcess.ServiceBase.Run(servicesToRun);
+		}
+
+		public Service()
+		{
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			timer = new Timer();
+			timer.Interval = 2500; //1000 * 60 * 60 * 24; // Set interval of one day 
+			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+			start_timer();
+			Console.Read();
+		}
+
+		static void timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			Console.WriteLine("hello");
+		}
+
+		private static void start_timer()
+		{
+			timer.Start();
+		}
+
+		protected override void OnStop()
+		{
+			timer.Stop();
+		}
+	}
+}
