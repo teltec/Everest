teltec/Everest

commit da2102755e732521cc169a7227eba20f97f889c1

CA1060: Move P/Invokes to NativeMethods class.

Teltec.FileSystem/NetworkDriveMapper.cs
@@ -13,19 +13,19 @@ namespace Teltec.FileSystem
 		{
 			localDriveName = FileManager.GetDriveLetter(localDriveName);
 
-			var resource = new NETRESOURCE
+			var resource = new NativeMethods.NETRESOURCE
 			{
-				dwType = ResourceType.DISK,
+				dwType = NativeMethods.ResourceType.DISK,
 				lpLocalName = localDriveName,
 				lpRemoteName = remotePath,
 				lpProvider = null
 			};
 
-			int ret = WNetUseConnection(
+			int ret = NativeMethods.WNetUseConnection(
 				IntPtr.Zero, resource,
 				promptUser ? null : password,
 				promptUser ? null : username,
-				promptUser ? Connect.INTERACTIVE | Connect.PROMPT : 0,
+				promptUser ? NativeMethods.Connect.INTERACTIVE | NativeMethods.Connect.PROMPT : 0,
 				null, null, null);
 
 			if (ret != NO_ERROR)
@@ -34,107 +34,113 @@ namespace Teltec.FileSystem
 
 		public static void UnmountNetworkLocation(string localDriveNameOrRemotePath)
 		{
-			int ret = WNetCancelConnection2(localDriveNameOrRemotePath, Connect.UPDATE_PROFILE, false);
+			int ret = NativeMethods.WNetCancelConnection2(localDriveNameOrRemotePath, NativeMethods.Connect.UPDATE_PROFILE, false);
 			if (ret != NO_ERROR)
 				throw new Win32Exception(ret);
 		}
 
 		#region Network PInvoke
 
+		internal static class NativeMethods
+		{
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			[DllImport("Mpr.dll")]
+			public static extern int WNetUseConnection
+			(
+				IntPtr hwndOwner,
+				NETRESOURCE lpNetResource,
+				string lpPassword,
+				string lpUserID,
+				Connect dwFlags,
+
+				[MarshalAs(UnmanagedType.LPWStr)] // Do this for all the following string fields
+				string lpAccessName,
+				string lpBufferSize,
+				string lpResult
+			);
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.wnetcancelconnection2
+			[DllImport("mpr.dll")]
+			public static extern int WNetCancelConnection2(string lpName, Connect dwFlags, bool bForce);
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			public enum ResourceScope
+			{
+				CONNECTED = 0x00000001,
+				GLOBALNET = 0x00000002,
+				REMEMBERED = 0x00000003,
+			}
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			public enum ResourceType
+			{
+				ANY = 0x00000000,
+				DISK = 0x00000001,
+				PRINT = 0x00000002,
+			}
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			public enum ResourceDisplayType
+			{
+				GENERIC = 0x00000000,
+				DOMAIN = 0x00000001,
+				SERVER = 0x00000002,
+				SHARE = 0x00000003,
+				FILE = 0x00000004,
+				GROUP = 0x00000005,
+				NETWORK = 0x00000006,
+				ROOT = 0x00000007,
+				SHAREADMIN = 0x00000008,
+				DIRECTORY = 0x00000009,
+				TREE = 0x0000000A,
+				NDSCONTAINER = 0x0000000A,
+			}
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			[Flags]
+			public enum ResourceUsage
+			{
+				CONNECTABLE = 0x00000001,
+				CONTAINER = 0x00000002,
+				NOLOCALDEVICE = 0x00000004,
+				SIBLING = 0x00000008,
+				ATTACHED = 0x00000010,
+			}
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			[Flags]
+			public enum Connect
+			{
+				UPDATE_PROFILE = 0x00000001,
+				INTERACTIVE = 0x00000008,
+				PROMPT = 0x00000010,
+				REDIRECT = 0x00000080,
+				LOCALDRIVE = 0x00000100,
+				COMMANDLINE = 0x00000800,
+				CMD_SAVECRED = 0x00001000,
+			}
+
+			// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
+			[StructLayout(LayoutKind.Sequential)]
+			public class NETRESOURCE
+			{
+				public ResourceScope dwScope = 0;
+				public ResourceType dwType = 0;
+				public ResourceDisplayType dwDisplayType = 0;
+				public ResourceUsage dwUsage = 0;
+
+				[MarshalAs(UnmanagedType.LPWStr)] // Do this for all the following string fields
+				public string lpLocalName = "";
+				public string lpRemoteName = "";
+				public string lpComment = "";
+				public string lpProvider = "";
+			}
+		}
+
 		public const int NO_ERROR = 0;
 		public const int ERROR_ALREADY_ASSIGNED = 85;
 
 		string errorMessage = new Win32Exception(Marshal.GetLastWin32Error()).Message;
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		[DllImport("Mpr.dll")]
-		private static extern int WNetUseConnection
-		(
-			IntPtr hwndOwner,
-			NETRESOURCE lpNetResource,
-			string lpPassword,
-			string lpUserID,
-			Connect dwFlags,
-			string lpAccessName,
-			string lpBufferSize,
-			string lpResult
-		);
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.wnetcancelconnection2
-		[DllImport("mpr.dll")]
-		static extern int WNetCancelConnection2(string lpName, Connect dwFlags, bool bForce);
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		public enum ResourceScope
-		{
-			CONNECTED = 0x00000001,
-			GLOBALNET = 0x00000002,
-			REMEMBERED = 0x00000003,
-		}
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		public enum ResourceType
-		{
-			ANY = 0x00000000,
-			DISK = 0x00000001,
-			PRINT = 0x00000002,
-		}
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		public enum ResourceDisplayType
-		{
-			GENERIC = 0x00000000,
-			DOMAIN = 0x00000001,
-			SERVER = 0x00000002,
-			SHARE = 0x00000003,
-			FILE = 0x00000004,
-			GROUP = 0x00000005,
-			NETWORK = 0x00000006,
-			ROOT = 0x00000007,
-			SHAREADMIN = 0x00000008,
-			DIRECTORY = 0x00000009,
-			TREE = 0x0000000A,
-			NDSCONTAINER = 0x0000000A,
-		}
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		[Flags]
-		public enum ResourceUsage
-		{
-			CONNECTABLE = 0x00000001,
-			CONTAINER = 0x00000002,
-			NOLOCALDEVICE = 0x00000004,
-			SIBLING = 0x00000008,
-			ATTACHED = 0x00000010,
-		}
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		[Flags]
-		public enum Connect
-		{
-			UPDATE_PROFILE = 0x00000001,
-			INTERACTIVE = 0x00000008,
-			PROMPT = 0x00000010,
-			REDIRECT = 0x00000080,
-			LOCALDRIVE = 0x00000100,
-			COMMANDLINE = 0x00000800,
-			CMD_SAVECRED = 0x00001000,
-		}
-
-		// REFERENCE http://www.pinvoke.net/default.aspx/mpr.WNetUseConnection
-		[StructLayout(LayoutKind.Sequential)]
-		private class NETRESOURCE
-		{
-			public ResourceScope dwScope = 0;
-			public ResourceType dwType = 0;
-			public ResourceDisplayType dwDisplayType = 0;
-			public ResourceUsage dwUsage = 0;
-
-			public string lpLocalName = "";
-			public string lpRemoteName = "";
-			public string lpComment = "";
-			public string lpProvider = "";
-		}
 
 		#endregion
 	}

Teltec.Backup.Scheduler/ServiceInstaller.cs
@@ -1,4 +1,4 @@
-﻿using NLog;
+using NLog;
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
@@ -26,43 +26,47 @@ namespace Teltec.Backup.Scheduler
 		public static readonly int STANDARD_RIGHTS_REQUIRED = 0xF0000;
 		public static readonly int SERVICE_WIN32_OWN_PROCESS = 0x00000010;
 
-		#region Memory
-
-		[DllImport("kernel32.dll", EntryPoint = "RtlFillMemory", SetLastError = false)]
-		public static extern void FillMemory(IntPtr destination, uint length, byte fill);
-
-		#endregion
-
 		#region Service
 
-		[DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
-		public static extern IntPtr OpenSCManager(string machineName, string databaseName, ScmAccessRights dwDesiredAccess);
+		internal static class NativeMethods
+		{
+			#region Memory
 
-		[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-		public static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, ServiceAccessRights dwDesiredAccess);
+			[DllImport("kernel32.dll", EntryPoint = "RtlFillMemory", SetLastError = false)]
+			public static extern void FillMemory(IntPtr destination, uint length, byte fill);
 
-		[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-		public static extern IntPtr CreateService(IntPtr hSCManager, string lpServiceName, string lpDisplayName, ServiceAccessRights dwDesiredAccess, int dwServiceType, ServiceBootFlag dwStartType, ServiceError dwErrorControl, string lpBinaryPathName, string lpLoadOrderGroup, IntPtr lpdwTagId, string lpDependencies, string lpServiceStartName, string lpPassword);
+			#endregion
 
-		[DllImport("advapi32.dll", SetLastError = true)]
-		[return: MarshalAs(UnmanagedType.Bool)]
-		public static extern bool CloseServiceHandle(IntPtr hSCObject);
+			[DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+			public static extern IntPtr OpenSCManager(string machineName, string databaseName, ScmAccessRights dwDesiredAccess);
 
-		[DllImport("advapi32.dll")]
-		public static extern int QueryServiceStatus(IntPtr hService, ServiceStatus lpServiceStatus);
+			[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+			public static extern IntPtr OpenService(IntPtr hSCManager, string lpServiceName, ServiceAccessRights dwDesiredAccess);
 
-		[DllImport("advapi32.dll", SetLastError = true)]
-		public static extern bool SetServiceStatus(IntPtr hService, ref ServiceStatus lpServiceStatus);
+			[DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+			public static extern IntPtr CreateService(IntPtr hSCManager, string lpServiceName, string lpDisplayName, ServiceAccessRights dwDesiredAccess, int dwServiceType, ServiceBootFlag dwStartType, ServiceError dwErrorControl, string lpBinaryPathName, string lpLoadOrderGroup, IntPtr lpdwTagId, string lpDependencies, string lpServiceStartName, string lpPassword);
 
-		[DllImport("advapi32.dll", SetLastError = true)]
-		[return: MarshalAs(UnmanagedType.Bool)]
-		public static extern bool DeleteService(IntPtr hService);
+			[DllImport("advapi32.dll", SetLastError = true)]
+			[return: MarshalAs(UnmanagedType.Bool)]
+			public static extern bool CloseServiceHandle(IntPtr hSCObject);
 
-		[DllImport("advapi32.dll")]
-		public static extern int ControlService(IntPtr hService, ServiceControl dwControl, ServiceStatus lpServiceStatus);
+			[DllImport("advapi32.dll")]
+			public static extern int QueryServiceStatus(IntPtr hService, ServiceStatus lpServiceStatus);
 
-		[DllImport("advapi32.dll", SetLastError = true)]
-		public static extern int StartService(IntPtr hService, int dwNumServiceArgs, int lpServiceArgVectors);
+			[DllImport("advapi32.dll", SetLastError = true)]
+			public static extern bool SetServiceStatus(IntPtr hService, ref ServiceStatus lpServiceStatus);
+
+			[DllImport("advapi32.dll", SetLastError = true)]
+			[return: MarshalAs(UnmanagedType.Bool)]
+			public static extern bool DeleteService(IntPtr hService);
+
+			[DllImport("advapi32.dll")]
+			public static extern int ControlService(IntPtr hService, ServiceControl dwControl, ServiceStatus lpServiceStatus);
+
+			[DllImport("advapi32.dll", SetLastError = true)]
+			[return: MarshalAs(UnmanagedType.Bool)]
+			public static extern bool StartService(IntPtr hService, int dwNumServiceArgs, int lpServiceArgVectors);
+		}
 
 		#endregion
 
@@ -87,24 +91,24 @@ namespace Teltec.Backup.Scheduler
 
 			try
 			{
-				IntPtr service = OpenService(scm, ServiceName, ServiceAccessRights.AllAccess);
+				IntPtr service = NativeMethods.OpenService(scm, ServiceName, ServiceAccessRights.AllAccess);
 				if (service == IntPtr.Zero)
 					throw new ApplicationException("Service not installed.");
 
 				try
 				{
 					StopService(service);
-					if (!DeleteService(service))
+					if (!NativeMethods.DeleteService(service))
 						throw new ApplicationException("Could not delete service: " + GetLastErrorMessage());
 				}
 				finally
 				{
-					CloseServiceHandle(service);
+					NativeMethods.CloseServiceHandle(service);
 				}
 			}
 			finally
 			{
-				CloseServiceHandle(scm);
+				NativeMethods.CloseServiceHandle(scm);
 			}
 		}
 
@@ -114,17 +118,17 @@ namespace Teltec.Backup.Scheduler
 
 			try
 			{
-				IntPtr service = OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus);
+				IntPtr service = NativeMethods.OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus);
 
 				if (service == IntPtr.Zero)
 					return false;
 
-				CloseServiceHandle(service);
+				NativeMethods.CloseServiceHandle(service);
 				return true;
 			}
 			finally
 			{
-				CloseServiceHandle(scm);
+				NativeMethods.CloseServiceHandle(scm);
 			}
 		}
 
@@ -154,14 +158,14 @@ namespace Teltec.Backup.Scheduler
 
 			try
 			{
-				IntPtr service = OpenService(scm, ServiceName, ServiceAccessRights.AllAccess);
+				IntPtr service = NativeMethods.OpenService(scm, ServiceName, ServiceAccessRights.AllAccess);
 
 				// Create it if it doesn't exist yet.
 				if (service == IntPtr.Zero)
 				{
 					string lpDependencies = BuildDependencies();
 
-					service = CreateService(
+					service = NativeMethods.CreateService(
 						scm, ServiceName, DisplayName,
 						DesiredAccess, ServiceType,
 						StartType, ErrorControl, BinaryPath,
@@ -177,12 +181,12 @@ namespace Teltec.Backup.Scheduler
 				}
 				finally
 				{
-					CloseServiceHandle(service);
+					NativeMethods.CloseServiceHandle(service);
 				}
 			}
 			finally
 			{
-				CloseServiceHandle(scm);
+				NativeMethods.CloseServiceHandle(scm);
 			}
 		}
 
@@ -192,7 +196,7 @@ namespace Teltec.Backup.Scheduler
 
 			try
 			{
-				IntPtr service = OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus | ServiceAccessRights.Start);
+				IntPtr service = NativeMethods.OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus | ServiceAccessRights.Start);
 				if (service == IntPtr.Zero)
 					throw new ApplicationException("Could not open service.");
 
@@ -202,12 +206,12 @@ namespace Teltec.Backup.Scheduler
 				}
 				finally
 				{
-					CloseServiceHandle(service);
+					NativeMethods.CloseServiceHandle(service);
 				}
 			}
 			finally
 			{
-				CloseServiceHandle(scm);
+				NativeMethods.CloseServiceHandle(scm);
 			}
 		}
 
@@ -217,7 +221,7 @@ namespace Teltec.Backup.Scheduler
 
 			try
 			{
-				IntPtr service = OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus | ServiceAccessRights.Stop);
+				IntPtr service = NativeMethods.OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus | ServiceAccessRights.Stop);
 				if (service == IntPtr.Zero)
 					throw new ApplicationException("Could not open service.");
 
@@ -227,21 +231,25 @@ namespace Teltec.Backup.Scheduler
 				}
 				finally
 				{
-					CloseServiceHandle(service);
+					NativeMethods.CloseServiceHandle(service);
 				}
 			}
 			finally
 			{
-				CloseServiceHandle(scm);
+				NativeMethods.CloseServiceHandle(scm);
 			}
 		}
 
 		private void StartService(IntPtr service)
 		{
 			ServiceStatus status = new ServiceStatus();
-			int result = StartService(service, 0, 0);
-			if (result == 0)
-				logger.Error(GetLastErrorMessage());
+			bool result = NativeMethods.StartService(service, 0, 0);
+			if (!result)
+			{
+				string message = GetLastErrorMessage();
+				logger.Error(message);
+				throw new ApplicationException(message);
+			}
 
 			var changedStatus = WaitForServiceStatus(service, ServiceState.StartPending, ServiceState.Running);
 			if (!changedStatus)
@@ -251,7 +259,7 @@ namespace Teltec.Backup.Scheduler
 		private void StopService(IntPtr service)
 		{
 			ServiceStatus status = new ServiceStatus();
-			ControlService(service, ServiceControl.Stop, status);
+			NativeMethods.ControlService(service, ServiceControl.Stop, status);
 			var changedStatus = WaitForServiceStatus(service, ServiceState.StopPending, ServiceState.Stopped);
 			if (!changedStatus)
 				throw new ApplicationException("Unable to stop service");
@@ -263,7 +271,7 @@ namespace Teltec.Backup.Scheduler
 
 			try
 			{
-				IntPtr service = OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus);
+				IntPtr service = NativeMethods.OpenService(scm, ServiceName, ServiceAccessRights.QueryStatus);
 				if (service == IntPtr.Zero)
 					return ServiceState.NotFound;
 
@@ -273,12 +281,12 @@ namespace Teltec.Backup.Scheduler
 				}
 				finally
 				{
-					CloseServiceHandle(service);
+					NativeMethods.CloseServiceHandle(service);
 				}
 			}
 			finally
 			{
-				CloseServiceHandle(scm);
+				NativeMethods.CloseServiceHandle(scm);
 			}
 		}
 
@@ -286,7 +294,7 @@ namespace Teltec.Backup.Scheduler
 		{
 			ServiceStatus status = new ServiceStatus();
 
-			if (QueryServiceStatus(service, status) == 0)
+			if (NativeMethods.QueryServiceStatus(service, status) == 0)
 				throw new ApplicationException("Failed to query service status.");
 
 			return status.dwCurrentState;
@@ -310,7 +318,7 @@ namespace Teltec.Backup.Scheduler
 		{
 			ServiceStatus status = new ServiceStatus();
 
-			QueryServiceStatus(service, status);
+			NativeMethods.QueryServiceStatus(service, status);
 			if (status.dwCurrentState == desiredStatus)
 				return true;
 
@@ -334,7 +342,7 @@ namespace Teltec.Backup.Scheduler
 
 				// Check the status again.
 
-				if (QueryServiceStatus(service, status) == 0)
+				if (NativeMethods.QueryServiceStatus(service, status) == 0)
 				{
 					logger.Error(GetLastErrorMessage());
 					break;
@@ -372,7 +380,7 @@ namespace Teltec.Backup.Scheduler
 
 		private IntPtr OpenSCManager(ScmAccessRights rights)
 		{
-			IntPtr scm = OpenSCManager(MachineName, null, rights);
+			IntPtr scm = NativeMethods.OpenSCManager(MachineName, null, rights);
 			if (scm == IntPtr.Zero)
 				throw new ApplicationException("Could not connect to service control manager: " + GetLastErrorMessage());
 

Teltec.Common/NativeMethods.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 
 namespace Teltec.Common
@@ -21,22 +21,22 @@ namespace Teltec.Common
 		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
 		public struct TV_ITEM
 		{
-			public int mask;
-			public IntPtr hItem;
-			public int state;
-			public int stateMask;
-			public IntPtr /* LPTSTR */ pszText;
-			public int cchTextMax;
-			public int iImage;
-			public int iSelectedImage;
-			public int cChildren;
-			public IntPtr lParam;
+			internal int mask;
+			internal IntPtr hItem;
+			internal int state;
+			internal int stateMask;
+			internal IntPtr /* LPTSTR */ pszText;
+			internal int cchTextMax;
+			internal int iImage;
+			internal int iSelectedImage;
+			internal int cChildren;
+			internal IntPtr lParam;
 		}
 
 		[DllImport("user32.dll", CharSet = CharSet.Auto)]
-		public static extern IntPtr SendMessage(HandleRef hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+		internal static extern IntPtr SendMessage(HandleRef hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
 		[DllImport("user32.dll", CharSet = CharSet.Auto)]
-		public static extern IntPtr SendMessage(HandleRef hWnd, int Msg, IntPtr wParam, ref TV_ITEM lParam);
+		internal static extern IntPtr SendMessage(HandleRef hWnd, int Msg, IntPtr wParam, ref TV_ITEM lParam);
 	}
 }

Teltec.Common/Unmanaged.cs
@@ -6,14 +6,22 @@ namespace Teltec.Common
 	{
 		#region Trap console application events
 
-		/// <summary>
-		/// Adds or removes an application-defined HandlerRoutine function from the list of handler functions for the calling process
-		/// </summary>
-		/// <param name="handler">A pointer to the application-defined HandlerRoutine function to be added or removed. This parameter can be NULL.</param>
-		/// <param name="add">If this parameter is TRUE, the handler is added; if it is FALSE, the handler is removed.</param>
-		/// <returns>If the function succeeds, the return value is true.</returns>
-		[DllImport("Kernel32")]
-		public static extern bool SetConsoleCtrlHandler(HandlerRoutine handler, bool add);
+		internal static class NativeMethods
+		{
+			/// <summary>
+			/// Adds or removes an application-defined HandlerRoutine function from the list of handler functions for the calling process
+			/// </summary>
+			/// <param name="handler">A pointer to the application-defined HandlerRoutine function to be added or removed. This parameter can be NULL.</param>
+			/// <param name="add">If this parameter is TRUE, the handler is added; if it is FALSE, the handler is removed.</param>
+			/// <returns>If the function succeeds, the return value is true.</returns>
+			[DllImport("Kernel32")]
+			public static extern bool SetConsoleCtrlHandler(HandlerRoutine handler, bool add);
+		}
+
+		public static bool SetConsoleCtrlHandler(HandlerRoutine handler, bool add)
+		{
+			return NativeMethods.SetConsoleCtrlHandler(handler, add);
+		}
 
 		/// <summary>
 		/// The console close handler delegate.

commit 1874d841e61d17aff81e0137d5ecf464653f321a

Fix bug introduced by da21027.

Teltec.FileSystem/NetworkDriveMapper.cs
@@ -53,7 +53,7 @@ namespace Teltec.FileSystem
 				string lpUserID,
 				Connect dwFlags,
 
-				[MarshalAs(UnmanagedType.LPWStr)] // Do this for all the following string fields
+				//[MarshalAs(UnmanagedType.LPWStr)] // Using `LPWStr` makes  `WNetUseConnection` return error code 1200.
 				string lpAccessName,
 				string lpBufferSize,
 				string lpResult
@@ -129,7 +129,7 @@ namespace Teltec.FileSystem
 				public ResourceDisplayType dwDisplayType = 0;
 				public ResourceUsage dwUsage = 0;
 
-				[MarshalAs(UnmanagedType.LPWStr)] // Do this for all the following string fields
+				//[MarshalAs(UnmanagedType.LPWStr)] // Using `LPWStr` makes  `WNetUseConnection` return error code 1200.
 				public string lpLocalName = "";
 				public string lpRemoteName = "";
 				public string lpComment = "";